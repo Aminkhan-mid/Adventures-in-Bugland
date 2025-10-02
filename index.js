de777c8d1f1b44bfc3274b40222cbc80ab6458a9
Amin Khan <154885880+Aminkhan-mid@users.noreply.github.com>
2025-10-02 15:29:34 +0530

    made most of the css for mobiles.

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
-fetch(`https://pokeapi.co/api/v2/pokemon/pikachu`)
+let id = Math.floor(Math.random() * 626)
+
+
+fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
     .then(res => res.json())
     .then(data => {
-        console.log(data.sprites)
+        console.log(data)
         let gif =  data.sprites.versions["generation-v"]["black-white"].animated.front_default || data.sprites.front_default
         document.getElementById("pokemon").innerHTML = `<img class="pokemonGif" src="${gif}">`
     })
